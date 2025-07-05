db248fe4729c61c94472b1d3a94d4128051c831e
Alexander Andrianov <an4lex@gmail.com>
2025-07-06 02:50:23 +0300

    Initial Commit

---
+++ LexiGrow/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  LexiGrow
+//
+//  Created by Alexander Andrianov on 20.06.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    Image(.appLogo)
+  }
+}
+
+#Preview {
+  ContentView()
+}
---
+++ LexiGrow/LexiGrowApp.swift
@@ -0,0 +1,17 @@
+//
+//  LexiGrowApp.swift
+//  LexiGrow
+//
+//  Created by Alexander Andrianov on 20.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct LexiGrowApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
