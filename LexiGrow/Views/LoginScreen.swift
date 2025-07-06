0ef4f90c205be05436b707b11efb82b10be78608
Alexander Andrianov <an4lex@gmail.com>
2025-07-06 17:09:50 +0300

    UI improvements for the login and registration screens.

--- LexiGrow/Views/LoginScreen.swift
+++ LexiGrow/Views/LoginScreen.swift
@@ -12,51 +12,127 @@ struct LoginScreen: View {
   
   var body: some View {
     NavigationStack {
-      VStack(spacing: 30) {
-        Text("Login")
-          .font(.title)
-          .fontWeight(.semibold)
-        
-        VStack(spacing: 15) {
-          TextField("Email", text: $viewModel.email)
-            .customInputFieldStyle()
-          SecureField("Password", text: $viewModel.password)
-            .customInputFieldStyle()
-        }.padding(.horizontal)
-        
-        Button {
-          viewModel.signIn()
-        } label: {
-          Text("Sign In")
-            .font(.callout)
-            .fontWeight(.medium)
-            .foregroundStyle(.white)
-            .padding(.vertical)
-            .padding(.horizontal, 160)
-            .background(.green)
-            .clipShape(.rect(cornerRadius: 15))
-        }
-        .disabled(!viewModel.isValidForm)
-        .opacity(!viewModel.isValidForm ? 0.5 : 1)
-        
-        HStack {
-          Text("Don't have have an account?")
-            .font(.footnote)
-            .foregroundStyle(.secondary)
-          NavigationLink {
-            RegistrationScreen()
-          } label: {
-            Text("Sign Up")
-          }
-          .fontWeight(.medium)
+      VStack(spacing:30) {
+        title
+        inputFields
+        signInButton
+        signUpOption
+        Spacer()
+        TypingEffectView()
+      }.padding(.top)
+    }
+  }
+  
+  // MARK: - Subviews
+  
+  private var title: some View {
+    Text("Login")
+      .font(.title2)
+      .fontWeight(.semibold)
+      .fontDesign(.monospaced)
+  }
+  
+  private var inputFields: some View {
+    VStack(spacing:15) {
+      TextField("Email", text: $viewModel.email)
+        .customInputFieldStyle()
+      SecureField("Password", text: $viewModel.password)
+        .customInputFieldStyle()
+    }
+    .fontDesign(.monospaced)
+    .padding(.horizontal)
+  }
+  
+  private var signInButton: some View {
+    Button {
+      viewModel.signIn()
+    } label: {
+      Text("Sign In")
+        .font(.callout)
+        .fontWeight(.medium)
+        .fontDesign(.monospaced)
+        .foregroundStyle(.white)
+        .padding(.vertical)
+        .padding(.horizontal,150)
+        .background(.indigo)
+        .clipShape(.rect(cornerRadius:15))
+    }
+    .disabled(!viewModel.isValidForm)
+    .opacity(!viewModel.isValidForm ? 0.5 : 1.0)
+  }
+  
+  private var signUpOption: some View {
+    HStack {
+      Text("Don't have have an account?")
+        .font(.footnote)
+        .foregroundStyle(.secondary)
+      NavigationLink {
+        RegistrationScreen()
+      } label: {
+        Text("Sign Up")
           .font(.subheadline)
-          .foregroundStyle(.green)
-        }
+          .fontWeight(.medium)
+          .underline()
       }
+      .tint(.primary)
     }
   }
 }
 
 #Preview {
   LoginScreen()
+}
+
+
+struct TypingEffectView: View {
+  
+  let fullText = "Welcome to LexiGrow."
+  @State private var displayedText = ""
+  @State private var isDeleting = false
+  @State private var timer: Timer?
+  @State private var typingTextOpacity = 0.0
+  
+  var body: some View {
+    
+    HStack(spacing: 0) {
+        Text(displayedText)
+          .font(.footnote)
+          .fontWeight(.medium)
+          .fontDesign(.monospaced)
+          .underline()
+        Rectangle() // Cursor
+          .frame(width: 2, height: 20)
+      }
+      .onAppear {
+        startAnimation()
+      }
+      .onDisappear {
+        timer?.invalidate()
+      }
+      .foregroundStyle(displayedText == fullText ? .indigo : .gray)
+      .opacity(typingTextOpacity)
+  }
+  
+  private func startAnimation() {
+    timer = Timer.scheduledTimer(withTimeInterval: 0.15, repeats: true) { _ in
+      if isDeleting {
+        if !displayedText.isEmpty {
+          displayedText.removeLast()
+          typingTextOpacity -= 0.1
+        } else {
+          isDeleting = false
+        }
+      } else {
+        if displayedText.count < fullText.count {
+          let index = fullText.index(fullText.startIndex, offsetBy: displayedText.count)
+          displayedText.append(fullText[index])
+          typingTextOpacity += 0.1
+        } else {
+          DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+            isDeleting = true
+          }
+        }
+      }
+    }
+  }
 }
--- LexiGrow/Views/RegistrationScreen.swift
+++ LexiGrow/Views/RegistrationScreen.swift
@@ -8,60 +8,78 @@
 import SwiftUI
 
 struct RegistrationScreen: View {
-  
   @State var viewModel = RegistrationViewModel()
   @Environment(\.dismiss) var dismiss
   
   var body: some View {
-    
     ZStack {
-      // Add a background color here.
+      // Add a background color in the future.
       ScrollView {
-        VStack(spacing: 30) {
-          
-          Text("Registration")
-            .font(.title)
-            .fontWeight(.semibold)
-          
-          VStack(spacing: 15) {
-            TextField("Full Name", text: $viewModel.fullName)
-              .customInputFieldStyle()
-            TextField("Email", text: $viewModel.email)
-              .customInputFieldStyle()
-            SecureField("Password", text: $viewModel.password)
-              .customInputFieldStyle()
-            SecureField("Confirm Password", text: $viewModel.confirmedPassword)
-              .customInputFieldStyle()
-          }.padding(.horizontal)
-          
-          Button {
-            viewModel.signUp()
-          } label: {
-            Text("Sign Up")
-              .font(.callout)
-              .fontWeight(.medium)
-              .foregroundStyle(.white)
-              .padding(.vertical)
-              .padding(.horizontal, 160)
-              .background(.green)
-              .clipShape(.rect(cornerRadius: 15))
-          }
-          .disabled(!viewModel.isValidForm)
-          .opacity(!viewModel.isValidForm ? 0.5 : 1)
-          
-          HStack {
-            Text("Already have an account?")
-              .font(.footnote)
-              .foregroundStyle(.secondary)
-            Button("Sign In") {
-              dismiss()
-            }
-            .fontWeight(.medium)
-            .font(.subheadline)
-            .foregroundStyle(.green)
-          }
+        VStack(spacing:30) {
+          title
+          inputFields
+          signUpButton
+          signInOption
         }.padding(.top)
       }
+    }
+  }
+  
+  // MARK: - Subviews
+  
+  private var title: some View {
+    Text("Registration")
+      .font(.title2)
+      .fontWeight(.semibold)
+      .fontDesign(.monospaced)
+  }
+  
+  private var inputFields: some View {
+    VStack(spacing:15) {
+      TextField("Full Name", text: $viewModel.fullName)
+        .customInputFieldStyle()
+      TextField("Email", text: $viewModel.email)
+        .customInputFieldStyle()
+      SecureField("Password", text: $viewModel.password)
+        .customInputFieldStyle()
+      SecureField("Confirm Password", text: $viewModel.confirmedPassword)
+        .customInputFieldStyle()
+    }
+    .fontDesign(.monospaced)
+    .padding(.horizontal)
+  }
+  
+  private var signUpButton: some View {
+    Button {
+      viewModel.signUp()
+    } label: {
+      Text("Sign Up")
+        .font(.callout)
+        .fontWeight(.medium)
+        .fontDesign(.monospaced)
+        .foregroundStyle(.white)
+        .padding(.vertical)
+        .padding(.horizontal,160)
+        .background(.indigo)
+        .clipShape(.rect(cornerRadius:15))
+    }
+    .disabled(!viewModel.isValidForm)
+    .opacity(!viewModel.isValidForm ? 0.5 : 1)
+  }
+  
+  private var signInOption: some View {
+    HStack {
+      Text("Already have an account?")
+        .font(.footnote)
+        .foregroundStyle(.secondary)
+      Button {
+        dismiss()
+      } label: {
+        Text("Sign In")
+          .fontWeight(.medium)
+          .font(.subheadline)
+          .underline()
+      }.tint(.primary)
     }
   }
 }
