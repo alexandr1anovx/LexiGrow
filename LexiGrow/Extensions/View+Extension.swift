15d55a95342c1b6b968e7c0a0debed602ca2ed06
Alexander Andrianov <an4lex@gmail.com>
2025-07-06 15:28:19 +0300

    Added extension for View type for custom input field style.

---
+++ LexiGrow/Extensions/View+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  View+Extension.swift
+//  LexiGrow
+//
+//  Created by Alexander Andrianov on 06.07.2025.
+//
+
+import SwiftUI
+
+extension View {
+  func customInputFieldStyle() -> some View {
+    self
+      .padding()
+      .frame(minHeight: 55)
+      .overlay {
+        RoundedRectangle(cornerRadius: 15)
+          .stroke(.gray.opacity(0.5), lineWidth: 1)
+      }
+  }
+}
