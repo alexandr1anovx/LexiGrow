1a08d61d529a913bc552305170e22a5ee9609c36
Alexander Andrianov <an4lex@gmail.com>
2025-07-08 22:40:19 +0300

    Added a folder called "App" and put main app files there.

---
+++ LexiGrow/App/AppSessionManager.swift
@@ -0,0 +1,12 @@
+//
+//  AppSessionManager.swift
+//  LexiGrow
+//
+//  Created by Alexander Andrianov on 07.07.2025.
+//
+
+import Foundation
+
+@Observable final class AppSessionManager {
+  
+}
--- LexiGrow/App/LexiGrowApp.swift
+++ LexiGrow/App/LexiGrowApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct LexiGrowApp: App {
   var body: some Scene {
     WindowGroup {
-      ContentView()
+      AppTabView()
     }
   }
 }
