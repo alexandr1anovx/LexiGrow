84df7f369bb4cd702a19ae86a02590e2cbc2688b
Alexander Andrianov <an4lex@gmail.com>
2025-07-11 14:26:05 +0300

    UI improvements for the registration screen.

--- LexiGrow/Features/Authentication/Registration/RegistrationScreen.swift
+++ LexiGrow/Features/Authentication/Registration/RegistrationScreen.swift
@@ -7,9 +7,25 @@
 
 import SwiftUI
 
+enum InputFieldContent {
+  case username
+  case email
+  case password
+  case confirmPassword
+}
+
 struct RegistrationScreen: View {
-  @State var viewModel = RegistrationViewModel()
+  
+  @State var viewModel: RegistrationViewModel
+  @Environment(AuthManager.self) private var authManager
   @Environment(\.dismiss) var dismiss
+  
+  @State private var isShownPassword: Bool = false
+  @FocusState private var inputContent: InputFieldContent?
+  
+  init(authManager: AuthManager) {
+    _viewModel = State(wrappedValue: RegistrationViewModel(authManager: authManager))
+  }
   
   var body: some View {
     ScrollView {
@@ -29,35 +45,115 @@ struct RegistrationScreen: View {
   }
   
   private var inputFields: some View {
-    VStack(spacing:15) {
-      TextField("Full Name", text: $viewModel.fullName)
-        .customInputFieldStyle()
+    VStack(spacing:12) {
+      TextField("Username", text: $viewModel.username)
+        .focused($inputContent, equals: .username)
+        .textInputAutocapitalization(.never)
+        .autocorrectionDisabled(true)
+        .customInputFieldStyle() // repeats
+        .submitLabel(.next)
+        .onSubmit {
+          inputContent = .email
+        }
       TextField("Email", text: $viewModel.email)
-        .customInputFieldStyle()
-      SecureField("Password", text: $viewModel.password)
-        .customInputFieldStyle()
-      SecureField("Confirm Password", text: $viewModel.confirmedPassword)
-        .customInputFieldStyle()
+        .focused($inputContent, equals: .email)
+        .textInputAutocapitalization(.never)
+        .autocorrectionDisabled(true)
+        .keyboardType(.emailAddress)
+        .customInputFieldStyle() // repeats
+        .submitLabel(.next)
+        .onSubmit {
+          inputContent = .password
+        }
+      
+      Group {
+        if isShownPassword {
+          HStack {
+            TextField("Password", text: $viewModel.password)
+            Button {
+              isShownPassword.toggle()
+            } label: {
+              Image(systemName: "eye.slash")
+            }
+            .opacity(viewModel.password.count > 1 ? 1 : 0)
+          }
+        } else {
+          HStack {
+            SecureField("Password", text: $viewModel.password)
+            Button {
+              isShownPassword.toggle()
+            } label: {
+              Image(systemName: "eye")
+            }
+            .opacity(viewModel.password.count > 1 ? 1 : 0)
+          }
+        }
+      }
+      .focused($inputContent, equals: .password)
+      .textInputAutocapitalization(.never)
+      .autocorrectionDisabled(true)
+      .customInputFieldStyle() // repeats
+      .submitLabel(.next)
+      .onSubmit {
+        inputContent = .confirmPassword
+      }
+        
+      HStack {
+        SecureField("Confirm Password", text: $viewModel.confirmedPassword)
+        Button {
+          isShownPassword.toggle()
+        } label: {
+          Image(systemName: viewModel.password == viewModel.confirmedPassword ? "checkmark.seal.fill" : "xmark.seal.fill")
+            .foregroundStyle(viewModel.password == viewModel.confirmedPassword ? .green : .red)
+            .opacity(viewModel.confirmedPassword.count > 1 ? 1 : 0)
+        }
+      }
+      .focused($inputContent, equals: .confirmPassword)
+      .textInputAutocapitalization(.never)
+      .autocorrectionDisabled(true)
+      .customInputFieldStyle() // repeats
+      .submitLabel(.done)
+      .onSubmit {
+        inputContent = nil
+      }
     }
     .font(.subheadline)
-    .padding(.horizontal)
+    .padding(.horizontal,25)
   }
   
   private var signUpButton: some View {
     Button {
       viewModel.signUp()
     } label: {
-      Text("Sign Up")
-        .font(.subheadline)
-        .fontWeight(.medium)
-        .foregroundStyle(.background)
-        .padding(.vertical,18)
-        .padding(.horizontal,140)
-        .background(.primary)
-        .clipShape(.rect(cornerRadius: 14))
+      Group {
+        if authManager.isLoading {
+          HStack {
+            ProgressView()
+            Text("Registration ...")
+          }
+          .font(.callout)
+          .fontWeight(.medium)
+          .fontDesign(.monospaced)
+          .foregroundStyle(.white)
+          .padding(.vertical,16)
+          .padding(.horizontal,80)
+          .background(Color.gradientOrangePink)
+          .clipShape(.rect(cornerRadius:15))
+        } else {
+          Text("Sign Up")
+            .font(.callout)
+            .fontWeight(.medium)
+            .fontDesign(.monospaced)
+            .foregroundStyle(.white)
+            .padding(.vertical,16)
+            .padding(.horizontal,135)
+            .background(Color.gradientOrangePink)
+            .clipShape(.rect(cornerRadius:15))
+        }
+      }
+      .disabled(!viewModel.isValidForm)
+      .opacity(!viewModel.isValidForm ? 0.5 : 1)
     }
-    .disabled(!viewModel.isValidForm)
-    .opacity(!viewModel.isValidForm ? 0.5 : 1)
   }
   
   private var signInOption: some View {
@@ -68,7 +164,7 @@ struct RegistrationScreen: View {
       Button {
         dismiss()
       } label: {
-        Text("Sign In")
+        Text("Sign In.")
           .fontWeight(.medium)
           .font(.subheadline)
           .underline()
@@ -78,5 +174,6 @@ struct RegistrationScreen: View {
 }
 
 #Preview {
-  RegistrationScreen()
+  RegistrationScreen(authManager: AuthManager())
+    .environment(AuthManager())
 }
