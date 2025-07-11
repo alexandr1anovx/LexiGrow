6b535583115242a64baadfb71c3274e7e68cb167
Alexander Andrianov <an4lex@gmail.com>
2025-07-11 14:25:52 +0300

    UI improvements for the login screen.

--- LexiGrow/Features/Authentication/Login/LoginScreen.swift
+++ LexiGrow/Features/Authentication/Login/LoginScreen.swift
@@ -8,7 +8,14 @@
 import SwiftUI
 
 struct LoginScreen: View {
-  @State var viewModel = LoginViewModel()
+  
+  @Environment(AuthManager.self) private var authManager
+  @FocusState private var inputContent: InputFieldContent?
+  @State var viewModel: LoginViewModel
+  
+  init(authManager: AuthManager) {
+    _viewModel = State(wrappedValue: LoginViewModel(authManager: authManager))
+  }
   
   var body: some View {
     NavigationStack {
@@ -28,30 +35,62 @@ struct LoginScreen: View {
   }
   
   private var inputFields: some View {
-    VStack(spacing:15) {
+    VStack(spacing:12) {
       TextField("Email", text: $viewModel.email)
-        .customInputFieldStyle()
+        .focused($inputContent, equals: .email)
+        .textInputAutocapitalization(.never)
+        .autocorrectionDisabled(true)
+        .keyboardType(.emailAddress)
+        .customInputFieldStyle() // repeats
+        .submitLabel(.next)
+        .onSubmit {
+          inputContent = .password
+        }
       SecureField("Password", text: $viewModel.password)
+        .focused($inputContent, equals: .email)
         .customInputFieldStyle()
+        .submitLabel(.done)
+        .onSubmit {
+          inputContent = nil
+        }
     }
-    .padding(.horizontal)
+    .padding(.horizontal,25)
   }
   
   private var signInButton: some View {
     Button {
       viewModel.signIn()
     } label: {
-      Text("Sign In")
-        .font(.callout)
-        .fontWeight(.medium)
-        .foregroundStyle(.background)
-        .padding(.vertical,18)
-        .padding(.horizontal,140)
-        .background(.primary)
-        .clipShape(.rect(cornerRadius:14))
+      Group {
+        if authManager.isLoading {
+          HStack {
+            ProgressView()
+            Text("Checking ...")
+          }
+          .font(.callout)
+          .fontWeight(.medium)
+          .fontDesign(.monospaced)
+          .foregroundStyle(.white)
+          .padding(.vertical,16)
+          .padding(.horizontal,95)
+          .background(Color.gradientOrangePink)
+          .clipShape(.rect(cornerRadius:15))
+        } else {
+          Text("Sign In")
+            .font(.callout)
+            .fontWeight(.medium)
+            .fontDesign(.monospaced)
+            .foregroundStyle(.white)
+            .padding(.vertical,16)
+            .padding(.horizontal,135)
+            .background(Color.gradientOrangePink)
+            .clipShape(.rect(cornerRadius:15))
+        }
+      }
+      .shadow(radius: 5)
+      .disabled(!viewModel.isValidForm)
+      .opacity(!viewModel.isValidForm ? 0.5 : 1.0)
     }
-    .disabled(!viewModel.isValidForm)
-    .opacity(!viewModel.isValidForm ? 0.5 : 1.0)
   }
   
   private var signUpOption: some View {
@@ -60,9 +99,9 @@ struct LoginScreen: View {
         .font(.footnote)
         .foregroundStyle(.secondary)
       NavigationLink {
-        RegistrationScreen()
+        RegistrationScreen(authManager: authManager)
       } label: {
-        Text("Sign Up")
+        Text("Sign Up.")
           .font(.subheadline)
           .fontWeight(.medium)
           .underline()
@@ -70,8 +109,4 @@ struct LoginScreen: View {
       .tint(.primary)
     }
   }
-}
-
-#Preview {
-  LoginScreen()
 }
