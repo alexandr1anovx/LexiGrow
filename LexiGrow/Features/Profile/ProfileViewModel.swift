72db74175bbc22e3244cd79c0b8b94a17151ff0a
Alexander Andrianov <an4lex@gmail.com>
2025-07-13 23:17:24 +0300

    Major update for the profile view model.

--- LexiGrow/Features/Profile/ProfileViewModel.swift
+++ LexiGrow/Features/Profile/ProfileViewModel.swift
@@ -7,14 +7,58 @@
 
 import Foundation
 
-@Observable final class ProfileViewModel {
-  var name: String = User.mockUser.name
-  var surname: String = User.mockUser.surname
-  var email: String = User.mockUser.email
-  var username: String? = User.mockUser.username
-  var joinDate: String = User.mockUser.formattedDate
+@Observable
+@MainActor
+final class ProfileViewModel {
   
+  // MARK: - Published Properties
+  
+  var username: String = ""
+  var email: String = ""
   var isShownSignOutSheet: Bool = false
-  var isShownDeleteAccountSheet: Bool = false
-  var isShownLanguageAlert: Bool = false
+  var isShownLanguageSheet: Bool = false
+  
+  // MARK: - Computed Properties
+  
+  var formHasChanges: Bool {
+    guard let user = authManager.currentUser else { return false }
+    let changedUsername = username != user.username
+    let changedEmail = email != user.email
+    return changedUsername || changedEmail
+  }
+  
+  // MARK: - Dependencies
+  
+  private let authManager: AuthManager
+  
+  // MARK: - Init / Deinit
+  
+  init(authManager: AuthManager) {
+    self.authManager = authManager
+    print("✅ Profile View Model has been initialized.")
+    Task { await getUserData() }
+  }
+  deinit {
+    print("❌ Profile View Model has been deinitialized.")
+  }
+  
+  // MARK: - Public Methods
+  
+  func updateUser() {
+    Task {
+      await authManager.updateUser(username: username)
+    }
+  }
+  
+  func signOut() {
+    Task {
+      await authManager.signOut()
+    }
+  }
+  
+  func getUserData() async {
+    guard let user = authManager.currentUser else { return }
+    username = user.username
+    email = user.email
+  }
 }
