058d2cb9d48d4383f040b608c2f2c8223e942e37
Alexander Andrianov <an4lex@gmail.com>
2025-07-13 23:23:36 +0300

    Improved the flashcards view.

--- LexiGrow/Features/Lessons/FlashcardsView.swift
+++ LexiGrow/Features/Lessons/FlashcardsView.swift
@@ -8,84 +8,176 @@
 import SwiftUI
 
 struct FlashcardsView: View {
-  @State private var isShownExitSheet: Bool = true
+  @State private var isShowingExitSheet: Bool = false
+  @State private var isFlipped: Bool = false
   @Environment(\.dismiss) var dismiss
+  
+  @State var cardCounter: Int = 0
+  @State var cardOrigin: CGPoint = .zero
   
   var body: some View {
     NavigationView {
-      VStack {
-        Text("Flashcards View")
-          .font(.title2)
-          .fontWeight(.bold)
-      }
-      .padding()
-      .navigationTitle("Flashcards")
-      .navigationBarTitleDisplayMode(.large)
-      .toolbar {
-        ToolbarItem(placement: .topBarTrailing) {
-          Button {
-            isShownExitSheet = true
-          } label: {
-            Image(systemName: "xmark.circle.fill")
-              .font(.title)
-              .symbolRenderingMode(.hierarchical)
-              .foregroundStyle(.red)
+      ZStack {
+        Color.cmBlack
+        .ignoresSafeArea()
+        VStack(spacing: 10) {
+          
+          ZStack {
+            cardFront
+            cardBack
+              .opacity(isFlipped ? 1:0)
+          }
+          .padding()
+          .rotation3DEffect(
+            .degrees(isFlipped ? 180 : 0),
+            axis: (x: 0.0, y: 1.0, z: 0.0)
+          )
+          .onTapGesture {
+            withAnimation(.spring(response: 1, dampingFraction: 0.7)) {
+              isFlipped.toggle()
+            }
+          }
+          Spacer()
+          
+          randomizeButton
+            .padding(.vertical)
+          
+        }
+        .navigationTitle("Flashcards")
+        .navigationBarTitleDisplayMode(.large)
+        .toolbar {
+          ToolbarItem(placement: .topBarTrailing) {
+            Button {
+              isShowingExitSheet = true
+            } label: {
+              Image(systemName: "xmark.circle.fill")
+                .font(.title)
+                .symbolRenderingMode(.hierarchical)
+                .foregroundStyle(.pink)
+            }
           }
         }
-      }
-      .sheet(isPresented: $isShownExitSheet) {
-        exitPreview
+        .sheet(isPresented: $isShowingExitSheet) {
+          ExitLessonView($isShowingExitSheet) {
+            dismiss()
+          }
+        }
       }
     }
   }
   
-  private var exitPreview: some View {
-    VStack(spacing: 15) {
-      Spacer()
-      Text("Finish the lesson?")
-        .font(.title)
-        .fontWeight(.semibold)
-        .foregroundStyle(.white)
-      Text("All progress will be canceled.")
-        .font(.callout)
-        .fontWeight(.medium)
-        .foregroundStyle(.white.opacity(0.8))
-        .multilineTextAlignment(.center)
-        .padding(.horizontal)
-      Spacer()
-      HStack {
-        Button {
-          isShownExitSheet = false
-        } label: {
-          Text("Cancel")
-            .padding(11)
-            .foregroundStyle(.white)
-        }
-        .buttonBorderShape(.capsule)
-        .buttonStyle(.bordered)
-        Button {
-          dismiss()
-        } label: {
-          Text("Finish")
-            .padding(11)
-            .foregroundStyle(.red)
-            .fontWeight(.medium)
-        }
-        .tint(.white)
-        .buttonBorderShape(.capsule)
-        .buttonStyle(.borderedProminent)
-      }.padding(.bottom)
-    }
-    .frame(maxWidth: .infinity, maxHeight: .infinity)
-    .presentationBackground(
-      LinearGradient(
-        colors: [.red.opacity(0.8), .pink.opacity(0.8)],
-        startPoint: .leading,
-        endPoint: .trailing
+  // MARK: - Subviews
+  
+  private var cardFront: some View {
+    RoundedRectangle(cornerRadius: 40)
+      .fill(
+        LinearGradient(
+          colors: [.teal, .green],
+          startPoint: .topLeading,
+          endPoint: .bottomTrailing
+        )
       )
-    )
-    .presentationDetents([.height(250)])
-    .presentationCornerRadius(50)
+      .onPressingChanged { point in
+        if let point {
+          cardOrigin = point
+          cardCounter += 1
+        }
+      }
+      .modifier(
+        RippleEffect(
+          at: cardOrigin,
+          trigger: cardCounter
+        )
+      )
+      .shadow(radius: 10)
+      .overlay {
+        VStack(spacing: 20) {
+          Text("Word")
+            .font(.largeTitle)
+            .fontWeight(.semibold)
+          Text("Transcription")
+            .font(.title2)
+            .fontWeight(.semibold)
+        }
+        .foregroundStyle(.white)
+      }
+  }
+  
+  private var cardBack: some View {
+    RoundedRectangle(cornerRadius: 40)
+      .fill(
+        LinearGradient(
+          colors: [.purple, .blue],
+          startPoint: .topLeading,
+          endPoint: .bottomTrailing
+        )
+      )
+      .shadow(radius: 10)
+      .overlay {
+        VStack {
+          Spacer()
+          Text("Translation")
+            .font(.largeTitle)
+            .fontWeight(.semibold)
+          Spacer()
+          HStack(spacing: 25) {
+            repeatButton
+            knowButton
+          }.padding(.vertical)
+        }
+        .foregroundStyle(.white)
+      }
+      .rotation3DEffect(
+        .degrees(180),
+        axis: (x: 0.0, y: 1.0, z: 0.0)
+      )
+  }
+  
+  private var randomizeButton: some View {
+    Button {
+      
+    } label: {
+      Label("Randomize", systemImage: "arrow.trianglehead.2.counterclockwise")
+        .font(.title3)
+        .fontWeight(.medium)
+        .padding(10)
+    }
+    .tint(.blue)
+    .buttonStyle(.bordered)
+    .buttonBorderShape(.capsule)
+    .shadow(radius: 8)
+  }
+  
+  private var knowButton: some View {
+    Button {
+      
+    } label: {
+      Label("Know", systemImage: "checkmark.seal.fill")
+        .font(.title3)
+        //.foregroundStyle(.white)
+        .fontWeight(.medium)
+        .padding(10)
+    }
+    .tint(.teal)
+    .buttonStyle(.bordered)
+    .buttonBorderShape(.capsule)
+    .shadow(radius: 8)
+  }
+  
+  private var repeatButton: some View {
+    Button {
+      
+    } label: {
+      Label("Repeat", systemImage: "repeat")
+        .font(.title3)
+        //.foregroundStyle(.white)
+        .fontWeight(.medium)
+        .padding(11)
+    }
+    .tint(.teal)
+    .buttonStyle(.bordered)
+    .buttonBorderShape(.capsule)
+    .shadow(radius: 8)
   }
 }
 
