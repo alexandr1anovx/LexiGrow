de08879a5bace61d516c4316c0298128c90c41a9
Alexander Andrianov <an4lex@gmail.com>
2025-07-16 22:42:35 +0300

    Improved UI and code readability.

--- LexiGrow/Features/Lessons/Flashcards/FlashcardsSummaryView.swift
+++ LexiGrow/Features/Lessons/Flashcards/FlashcardsSummaryView.swift
@@ -9,52 +9,81 @@ import SwiftUI
 
 struct FlashcardsSummaryView: View {
   @Bindable var viewModel: FlashcardsViewModel
+  var dismissAction: () -> Void
   
   var body: some View {
     ZStack {
-      
+      Color.cmSystem.ignoresSafeArea()
       VStack(spacing: 40) {
-        Text("The lesson is over!")
-          .font(.title)
-          .fontWeight(.bold)
-        
-        VStack(spacing:15) {
-          Text("You know: **\(viewModel.knownWordsCount)** words")
-            .font(.title2)
-            .foregroundStyle(.green)
-          Text("Must be repeated: **\(viewModel.repetitionWords.count)** words")
-            .font(.title2)
-            .foregroundStyle(.orange)
-        }
-        
-        HStack(spacing:10) {
-          Button {
-            viewModel.resetLesson()
-          } label: {
-            Label("Try Again", systemImage: "repeat")
-              .padding(11)
-          }
-          .tint(.teal)
-          .buttonStyle(.bordered)
-          .buttonBorderShape(.capsule)
-          Button {
-            // go to the main screen
-          } label: {
-            Label("Finish", systemImage: "flag.pattern.checkered.2.crossed")
-              .padding(11)
-          }
-          .tint(.pink)
-          .buttonStyle(.bordered)
-          .buttonBorderShape(.capsule)
-        }
-        .font(.headline)
+        title
+        lessonResultDescription
+        HStack(spacing: 15) {
+          tryAgainButton
+          finishButton
+        }.font(.headline)
       }
     }
+  }
+  
+  // MARK: - Subviews
+  
+  private var title: some View {
+    HStack(spacing: 5) {
+      Text("The lesson")
+      Text("is over!")
+        .foregroundStyle(.pink)
+    }
+    .font(.title)
+    .fontWeight(.bold)
+  }
+  
+  private var lessonResultDescription: some View {
+    VStack(spacing: 15) {
+      HStack(spacing: 5) {
+        Text("You know:")
+        Text("\(viewModel.knownWordsCount)")
+          .fontWeight(.bold)
+          .foregroundStyle(.pink)
+        Text("/")
+        Text("\(viewModel.lessonCards.count) words")
+      }.font(.title2)
+      Text("To repeat: **\(viewModel.repetitionWords.count)** words")
+        .font(.title2)
+        .foregroundStyle(.gray)
+    }
+  }
+  
+  private var tryAgainButton: some View {
+    Button {
+      viewModel.resetLesson()
+    } label: {
+      Label("Try Again", systemImage: "repeat")
+        .padding(11)
+        .foregroundStyle(.cmSystem)
+    }
+    .tint(.cmReversed)
+    .buttonStyle(.borderedProminent)
+    .buttonBorderShape(.roundedRectangle(radius:20))
+    .shadow(radius: 10)
+  }
+  
+  private var finishButton: some View {
+    Button {
+      dismissAction() // returns to the lessons screen
+    } label: {
+      Label("Finish", systemImage: "flag.pattern.checkered.2.crossed")
+        .padding(11)
+    }
+    .tint(.pink)
+    .buttonStyle(.borderedProminent)
+    .buttonBorderShape(.roundedRectangle(radius:20))
+    .shadow(radius: 10)
   }
 }
 
 #Preview {
-  FlashcardsSummaryView(
-    viewModel: FlashcardsViewModel()
-  )
+    FlashcardsSummaryView(
+      viewModel: FlashcardsViewModel(),
+      dismissAction: {}
+    )
 }
