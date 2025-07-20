e80b3fef635afa560e2cd03b01a6be5ca882a371
Alexander Andrianov <an4lex@gmail.com>
2025-07-20 19:04:46 +0300

    The "FlashcardTryAgain" view has been improved and moved to a separate folder.

---
+++ LexiGrow/Features/Lessons/Flashcards/FlashcardTryAgain/FlashcardTryAgainView.swift
@@ -0,0 +1,82 @@
+//
+//  FlashcardsTryAgainView.swift
+//  LexiGrow
+//
+//  Created by Alexander Andrianov on 14.07.2025.
+//
+
+import SwiftUI
+
+struct FlashcardTryAgainView: View {
+  @Environment(FlashcardsViewModel.self) var viewModel
+  
+  var body: some View {
+    ZStack {
+      Color.mainBackgroundColor
+        .ignoresSafeArea()
+      VStack(spacing: 40) {
+        TitleView()
+        HStack(spacing: 20) {
+          ReturnHomeButton()
+          TryAgainButton()
+        }
+      }
+    }
+  }
+}
+
+#Preview {
+  FlashcardTryAgainView()
+}
+
+private extension FlashcardTryAgainView {
+  
+  struct TitleView: View {
+    var body: some View {
+      VStack(spacing: 15) {
+        MultiColoredText(text: "Try Again")
+          .font(.title)
+          .fontWeight(.semibold)
+        Text("Would you like to repeat the lesson?")
+          .font(.headline)
+      }
+    }
+  }
+  
+  struct ReturnHomeButton: View {
+    @Environment(FlashcardsViewModel.self) var viewModel
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+      Button {
+        dismiss()
+        viewModel.selectedLevel = nil
+        viewModel.selectedTopic = nil
+      } label: {
+        Text("No, return home")
+          .padding(11)
+      }
+      .prominentButtonStyle(
+        tint: .cmReversed,
+        textColor: .cmSystem
+      )
+    }
+  }
+  
+  struct TryAgainButton: View {
+    @Environment(FlashcardsViewModel.self) var viewModel
+    
+    var body: some View {
+      Button {
+        viewModel.startLesson(
+          level: viewModel.selectedLevel!,
+          topic: viewModel.selectedTopic!
+        )
+      } label: {
+        Text("Yes, try again")
+          .padding(11)
+      }
+      .prominentButtonStyle(tint: .pink)
+    }
+  }
+}
