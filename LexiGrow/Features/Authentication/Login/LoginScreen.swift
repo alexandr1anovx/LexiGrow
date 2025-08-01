954aff417833c54f965e235fe9e4fcb7f2ca4315
Alexander Andrianov <an4lex@gmail.com>
2025-08-01 22:03:23 +0300

    LoginScreen: Replaced computed properties for view components with structures.

--- LexiGrow/Features/Authentication/Login/LoginScreen.swift
+++ LexiGrow/Features/Authentication/Login/LoginScreen.swift
@@ -21,73 +21,19 @@ struct LoginScreen: View {
     NavigationView {
       ScrollView {
         VStack(spacing: 30) {
-          title
-          inputFields
-          signInButton
-          signUpOption
-        }.padding(.top)
-      }
-    }
-  }
-  
-  // MARK: - Subviews
-  
-  private var title: some View {
-    TypingTextEffect(text: "Login. Welcome to LexiGrow.")
-  }
-  
-  private var inputFields: some View {
-    VStack(spacing: 12) {
-      InputField(.standard, "Email", text: $viewModel.email)
-        .focused($inputContent, equals: .email)
-        .textInputAutocapitalization(.never)
-        .autocorrectionDisabled(true)
-        .keyboardType(.emailAddress)
-        .submitLabel(.next)
-        .onSubmit { inputContent = .password }
-      InputField(.password, "Password", text: $viewModel.password)
-        .focused($inputContent, equals: .email)
-        .submitLabel(.done)
-        .onSubmit { inputContent = nil }
-    }
-    .padding(.horizontal, 25)
-  }
-  
-  private var signInButton: some View {
-    Button {
-      viewModel.signIn()
-    } label: {
-      Group {
-        if authManager.isLoading {
-          HStack {
-            ProgressView()
-            Text("Checking ...")
+          TypingTextEffect(text: "Login. Welcome to LexiGrow.")
+          InputFields(viewModel: viewModel)
+          Group {
+            if authManager.isLoading {
+              GradientRingProgressView()
+            } else {
+              SignInButton(viewModel: viewModel)
+            }
           }
-        } else {
-          Text("Sign In")
+          SignUpOption()
         }
+        .padding(.top,30)
       }
-    }
-    
-    .padding(.horizontal,30)
-    .disabled(!viewModel.isValidForm)
-    .opacity(!viewModel.isValidForm ? 0.5 : 1)
-  }
-  
-  private var signUpOption: some View {
-    HStack {
-      Text("Don't have have an account?")
-        .font(.footnote)
-        .foregroundStyle(.secondary)
-      NavigationLink {
-        RegistrationScreen(authManager: authManager)
-      } label: {
-        Text("Sign Up.")
-          .font(.subheadline)
-          .fontWeight(.medium)
-          .underline()
-      }
-      .tint(.primary)
     }
   }
 }
@@ -95,4 +41,69 @@ struct LoginScreen: View {
 #Preview {
   LoginScreen(authManager: AuthManager())
     .environment(AuthManager())
+    .environment(LoginViewModel(authManager: AuthManager()))
+}
+
+
+extension LoginScreen {
+  
+  struct InputFields: View {
+    @Bindable var viewModel: LoginViewModel
+    @FocusState private var inputContent: InputFieldContent?
+    
+    var body: some View {
+      VStack(spacing: 12) {
+        InputField(.standard, "Email", text: $viewModel.email)
+          .focused($inputContent, equals: .email)
+          .textInputAutocapitalization(.never)
+          .autocorrectionDisabled(true)
+          .keyboardType(.emailAddress)
+          .submitLabel(.next)
+          .onSubmit { inputContent = .password }
+        InputField(.password, "Password", text: $viewModel.password)
+          .focused($inputContent, equals: .email)
+          .submitLabel(.done)
+          .onSubmit { inputContent = nil }
+      }
+      .padding(.horizontal, 25)
+    }
+  }
+  
+  struct SignInButton: View {
+    @Environment(AuthManager.self) var authManager
+    @Bindable var viewModel: LoginViewModel
+    
+    var body: some View {
+      Button {
+        viewModel.signIn()
+      } label: {
+        Text("Sign In")
+          .padding(.horizontal,120)
+          .padding(12)
+      }
+      .prominentButtonStyle(tint: .blue)
+      .disabled(!viewModel.isValidForm)
+    }
+  }
+  
+  struct SignUpOption: View {
+    @Environment(AuthManager.self) var authManager
+    
+    var body: some View {
+      HStack {
+        Text("Don't have have an account?")
+          .font(.footnote)
+          .foregroundStyle(.secondary)
+        NavigationLink {
+          RegistrationScreen(authManager: authManager)
+        } label: {
+          Text("Sign Up.")
+            .font(.subheadline)
+            .fontWeight(.medium)
+            .underline()
+        }
+        .tint(.primary)
+      }
+    }
+  }
 }
