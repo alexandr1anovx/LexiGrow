c1e3859de47ddb51cde53bd28199ed48e3c9cf93
Alexander Andrianov <an4lex@gmail.com>
2025-09-14 23:08:27 +0300

    Add new methods and properties to the lessons view model.

--- LexiGrow/Features/Lessons/LessonsViewModel.swift
+++ LexiGrow/Features/Lessons/LessonsViewModel.swift
@@ -6,12 +6,13 @@
 //
 
 import Foundation
+import SwiftData
 
 @Observable
 @MainActor
 final class LessonsViewModel {
-  
-  private(set) var lessons: [Lesson] = []
+  private(set) var lessons: [LessonEntity] = []
+  private(set) var levels: [LevelProgress] = []
   private(set) var isLoading = false
   private(set) var errorMessage: String?
   private let supabaseService: SupabaseServiceProtocol
@@ -20,15 +21,48 @@ final class LessonsViewModel {
     self.supabaseService = supabaseService
   }
   
-  func getLessons() async {
-    isLoading = true
-    defer { isLoading = false }
-    // check if there any lessons data so not to make a repeat API call
-    guard lessons.isEmpty else { return }
+  /// Loads lessons data from local storage.
+  func fetchLocalLessons(context: ModelContext) {
     do {
-      self.lessons = try await supabaseService.getLessons()
+      let descriptor = FetchDescriptor<LessonEntity>(sortBy: [SortDescriptor(\.title)])
+      self.lessons = try context.fetch(descriptor)
+      print("✅ Fetched \(lessons.count) lessons from local storage.")
     } catch {
-      errorMessage = "Failed to get lessons: \(error)"
+      errorMessage = "Failed to fetch lessons from local storage: \(error.localizedDescription)"
+      print("⚠️ Failed to fetch lessons from local storage: \(error)")
+    }
+  }
+  
+  /// Synchronizes data from server with the local database.
+  func syncData(context: ModelContext) async {
+    isLoading = true
+    errorMessage = nil
+    defer { isLoading = false }
+    
+    do {
+      let localDescriptor = FetchDescriptor<LessonEntity>()
+      let localLessons = try context.fetch(localDescriptor)
+      
+      if localLessons.isEmpty {
+        print("Local data is empty. Fetching data from server...")
+        let remoteLessonsDTO = try await supabaseService.getLessons()
+        print("Got the \(remoteLessonsDTO.count) lessons from server.")
+        
+        // Convert DTO into Entity and add to the context.
+        for lessonDTO in remoteLessonsDTO {
+          let lessonEntity = LessonEntity(from: lessonDTO)
+          context.insert(lessonEntity)
+        }
+        try context.save()
+        print("✅ New lessons successfully saved into local database.")
+      } else {
+        print("✅ Local data found. No network request is required.")
+      }
+      fetchLocalLessons(context: context)
+    } catch {
+      let errorDescription = "Sync error: \(error.localizedDescription)"
+      errorMessage = errorDescription
+      print("❌ \(errorDescription)")
     }
   }
 }
